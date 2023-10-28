627a1450a1a7cb12f8f30c6382542923c8dba543
logankud <logankud@protonmail.com>
2023-10-27 20:40:11 -0400

    initial commit

---
+++ create_table/cltv_summary/create_table.sql
@@ -0,0 +1,21 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS shopify_cltv(
+
+email STRING
+,frequency INT
+,recency INT
+, T INT
+,monetary_value DOUBLE
+, p_alive FLOAT
+,n_predicted_purchases_60 FLOAT
+, aov   DOUBLE
+,predicted_clv DOUBLE
+
+
+)
+PARTITIONED BY 
+(
+partition_date DATE 
+)
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY ',' 
+LOCATION 's3://prymal-analytics/shopify/cltv/'
