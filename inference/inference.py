a9d1e5c07af730b6c57d8c37d8a0963d8ff9c0ab
logankud <logankud@protonmail.com>
2023-10-30 20:30:31 -0400

    changed name of secret variable

--- inference/inference.py
+++ inference/inference.py
@@ -17,8 +17,8 @@ from loguru import logger
 from datetime import timedelta
 
 
-AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY=os.environ['AWS_ACCESS_SECRET']
 
 
 REGION = 'us-east-1'
